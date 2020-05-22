602be6f1a483c1d0c13fb3383f1c00db24d4aa40
Henrique Bruno <henrique.bruno.fa@gmail.com>
2020-05-22 17:12:16 -0300

    wip

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,35 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
+# SrBrahma fork
+
+## [2.0.0] - May 21, 2020
+
+Removed allow_signup option. Now always true.
+
+Now always redirects to localhost.
+
+Client must provide the redirectPort.
+
+Displays an error page on error, only redirects to user on success (as with a invalid redirectPort query param, it wouldn't have an port to send the error code).
+
+
+## [?.?.?] - May 4, 2020
+
+Hardcoded the scope parameter, so malicious applications that have access to the user machine can't mistake him under your application name to get further authorizations.
+
+Added max states length, so malicious people can't explode Vercel RAM with invalid requests.
+
+
+## [?.?.?] - April 30, 2020
+
+Moved to typescript with the necessary changes.
+
+Minor general changes.
+
+
+# Parent Repository original Change Log
+
 ## [1.1.0] - 2019-08-07
 ### Added
 - Refactor to support Zeit Now's 2.0 routing system.
--- api/[action].ts
+++ api/[action].ts
@@ -1,11 +1,6 @@
 // You must set these env vars in your Vercel:
 // GH_CLIENT_ID
 // GH_CLIENT_SECRET
-// USER_REDIRECT_URL
-
-// There could be a success redirect to a vercel static page, with the error or success message
-// in the body, and the axios would send to the user the token or error.
-// Not willing to do it right now. Maybe later. Probably not.
 
 require('dotenv').config(); // dotEnv used on development mode, getting the env vars from .env file.
 import { NowRequest, NowResponse } from '@now/node';
@@ -14,102 +9,147 @@ import axios from 'axios';
 import uid from 'uid-promise';
 
 
-const githubUrl = process.env.GH_HOST || 'github.com';
-const userRedirect = process.env.USER_REDIRECT_URL || 'https://www.google.com';
+// https://developer.github.com/apps/building-oauth-apps/understanding-scopes-for-oauth-apps/
+// "Note: Your OAuth App can request the scopes in the initial redirection. You can specify multiple
+// scopes by separating them with a space". Ex: 'repo gist'
+const scope = 'repo';
 
 
-// If needed, the states array could also have a port or a full redirect uri,
-// allowing users to set custom ports to localhost, for example;
+// state format is uidString_customRedirectPort.
+// We store the redirect port with the state to use less Vercel computation power (to
+// use indexOf instead of findIndex. It is way faster.)
 const states: string[] = [];
 const statesMaxLength = 10000; // A big number. May be changed.
 
-// https://developer.github.com/apps/building-oauth-apps/understanding-scopes-for-oauth-apps/
-// "Note: Your OAuth App can request the scopes in the initial redirection. You can specify multiple
-// scopes by separating them with a space".
-// Mutiple scope example: 'repo gist'
-const scope = 'repo';
+
 
 function redirect(res: NowResponse, location: string, bodyObj?: any) {
   res.status(302);
-  if (bodyObj)
-    res.setHeader('Location', `${location}?${querystring.stringify(bodyObj)}`);
-  else
-    res.setHeader('Location', location);
+  res.setHeader('Location', location + (bodyObj ? `?${querystring.stringify(bodyObj)}` : ''));
   res.send('');
 }
 
-function redirectToUser(res: NowResponse, bodyObj?: any) {
-  redirect(res, userRedirect, bodyObj);
+
+function redirectToUser(res: NowResponse, bodyObj: any, port: number) {
+  redirect(res, `http://localhost:${port}`, bodyObj);
 };
 
-async function login(req: NowRequest, res: NowResponse) {
-  const state = await uid(30);
 
-  // Just a safety measure to avoid the Vercel RAM exploding, if callbacks doesn't happen for some reason.
+
+async function login(req: NowRequest, res: NowResponse) {
+  const { redirectPort: redirectPortString } = req.query;
+
+  let redirectPort = Number(redirectPortString); // If undefined, returns NaN
+
+  if (redirectPort < 0 || redirectPort > 65535 || !Number.isInteger(redirectPort)) // If NaN, isNotInteger.
+    return errorPage(res, 'Invalid or missing redirect port.');
+
+
+  // To avoid the Vercel RAM exploding, if callbacks doesn't happen for a reason (aka evil users hehe)
   if (states.length >= statesMaxLength)
     states.shift();
 
+  const state = `${await uid(20)}_${redirectPort}`;
   states.push(state);
-  const { allow_signup } = req.query;
+
   const query = {
     client_id: process.env.GH_CLIENT_ID,
     state,
-    scope
+    scope,
   } as any;
 
-  if (allow_signup !== undefined)
-    query.allow_signup = allow_signup;
-
-  redirect(res, `https://${githubUrl}/login/oauth/authorize`, query);
+  redirect(res, `https://github.com/login/oauth/authorize`, query);
 };
 
 
 
 async function callback(req: NowRequest, res: NowResponse) {
-  // res.setHeader('Content-Type', 'text/html');
-  const { code, state } = req.query as any;
+  const { code, state } = req.query;
 
-  if (!code && !state) {
-    redirectToUser(res, { error: 'Provide code and state query param' });
-  } else if (!states.includes(state)) {
-    redirectToUser(res, { error: 'Unknown state' });
-  } else {
-    states.splice(states.indexOf(state), 1);
-    try {
-      const { status, data } = await axios({
-        method: 'POST',
-        url: `https://${githubUrl}/login/oauth/access_token`,
-        responseType: 'json',
-        data: {
-          client_id: process.env.GH_CLIENT_ID,
-          client_secret: process.env.GH_CLIENT_SECRET,
-          code
-        }
-      });
+  if (Array.isArray(code) || Array.isArray(state) || !code || !state)
+    return errorPage(res, 'Error 1: Invalid query');
 
-      if (status === 200) {
-        const qs = querystring.parse(data);
-        if (qs.error) {
-          redirectToUser(res, { error: qs.error_description });
-        } else {
-          redirectToUser(res, { token: qs.access_token });
-        }
-      } else {
-        redirectToUser(res, { error: 'GitHub server error.' });
+  // Find the state in states array
+  const stateIndex = states.indexOf(state);
+
+  if (stateIndex == -1)
+    return errorPage(res, 'Error 2: State not found');
+
+  // Remove the state from states array
+  states.splice(stateIndex, 1);
+
+  try {
+    const { status, data } = await axios({
+      method: 'POST',
+      url: `https://github.com/login/oauth/access_token`,
+      responseType: 'json',
+      data: {
+        client_id: process.env.GH_CLIENT_ID,
+        client_secret: process.env.GH_CLIENT_SECRET,
+        state,
+        code
       }
-    } catch (err) {
-      redirectToUser(res, { error: 'Please provide GH_CLIENT_ID and GH_CLIENT_SECRET as environment variables. (or GitHub might be down)' });
+    });
+
+    if (status === 200) {
+      const qs = querystring.parse(data);
+      if (qs.error) {
+        return errorPage(res, 'Error 3: ' + String(qs.error_description));
+      } else {
+        const port = Number(state.split('_')[1]);
+        return redirectToUser(res, { token: qs.access_token }, port);
+      }
+    } else {
+      return errorPage(res, 'Error 4: GitHub server error');
     }
   }
-};
-
-// api/[action].js
-// https://micro-github.*USERNAME*.now.sh/api/login
-// https://micro-github.*USERNAME*.now.sh/api/callback
-module.exports = (req: NowRequest, res: NowResponse) => {
-  if (req.query.action === "login") {
-    login(req, res);
-  } else if (req.query.action === "callback") {
-    callback(req, res);
+  catch (err) {
+    return errorPage(res, 'Error 5: An uknown error occurred, maybe GitHub server is down right now');
   }
 };
+
+
+
+// https://micro-github.*USERNAME*.now.sh/api/login
+// https://micro-github.*USERNAME*.now.sh/api/callback
+// [name].ts https://vercel.com/docs/v2/serverless-functions/introduction#path-segments
+module.exports = (req: NowRequest, res: NowResponse) => {
+  switch (req.query.action) {
+    case 'login':
+      login(req, res); break;
+    case 'callback':
+      callback(req, res); break;
+  }
+};
+
+
+
+function errorPage(res: NowResponse, errorMessage: string) {
+  res.send(`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>GitHub OAuth Error</title>
+  </head>
+
+  <body>
+    <p>[ ${errorMessage} ]</p>
+
+    <style>
+      * {
+        padding: 0;
+        margin: 0;
+      }
+      p {
+        margin-top: 14px;
+        font-size: 18px;
+        font-family: sans-serif;
+        text-align: center;
+      }
+    </style>
+  </body>
+</html>
+
+`);
+}
